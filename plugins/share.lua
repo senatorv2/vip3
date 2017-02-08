f9cc2f2d625233c5eea4ddf8ad08e30f74704659
senatorv2 <shpan@hi2.in>
2017-02-08 05:08:04 +0330

    Add files via upload

---
+++ plugins/share.lua
@@ -0,0 +1,13 @@
+local function run(msg, matches)
+ -  if matches[1] == 'id' then
+ -	 pm = group[tostring(msg.chat_id_)]['id']
+ -	 tg.sendMessage(msg.chat_id_, 0, 1,'<b>ID</b> ➣➣ ['..msg.from_id..']' , 1, 'html')
+ -	end
+ -    end
+ -return {
+ -  patterns = {
+ -    "^[#!/](id)$"
+ -  },
+ -  run = run
+ -}
+ -
