33d5ece970f5227b6da81a2b7fc178642b64d622
senatorv2 <shpan@hi2.in>
2017-02-14 20:10:04 +0330

    Update msg_checks.lua

--- plugins/msg_checks.lua
+++ plugins/msg_checks.lua
@@ -40,6 +40,21 @@ local is_sticker_msg = msg.text:match("!!!sticker:")
 if group_lock_sticker == 'yes' and is_sticker_msg then
 tg.deleteMessages(msg.chat_id_, {[0] = msg.id_ })
 end
+local group_lock_contact = group[tostring(msg.chat_id)]['settings']['lock_contact']
+local is_contact_msg = msg.text:match("!!!contact:")
+if group_lock_contact == 'yes' and is_contact_msg then
+tg.deleteMessages(msg.chat_id_, {[0] = msg.id_ })
+end   
+local group_lock_location = group[tostring(msg.chat_id)]['settings']['lock_location']
+local is_location_msg = msg.text:match("!!!location:")
+if group_lock_location == 'yes' and is_location_msg then
+tg.deleteMessages(msg.chat_id_, {[0] = msg.id_ })
+end
+local group_lock_game = group[tostring(msg.chat_id)]['settings']['lock_game']
+local is_game_msg = msg.text:match("!!!game:")
+if group_lock_game == 'yes' and is_game_msg then
+tg.deleteMessages(msg.chat_id_, {[0] = msg.id_ })
+end
 local group_lock_spam = group[tostring(msg.chat_id)]['settings']['lock_spam']
 local _nl, ctrl_chars = string.gsub(msg.text, '%c', '')
 local _nl, real_digits = string.gsub(msg.text, '%d', '')
@@ -80,7 +95,20 @@ local group_edit_lock = group[tostring(msg.chat_id)]['settings']['lock_edit']
 local is_edit_msg = msg.text:match("!!!edit:")
 if group_edit_lock == 'yes' and is_edit_msg then
 tg.deleteMessages(msg.chat_id_, {[0] = msg.id_ })
+end 
+local group_fosh_lock = group[tostring(msg.chat_id)]['settings']['lock_fosh']
+local is_fosh_msg = msg.text:match("مار") or msg.text:match("کونی") or msg.text:match("کیونی") or msg.text:match("کصده") or msg.text:match("تخمه") or msg.text:match("سگ") or msg.text:match("حروم زاده") or msg.text:match("حرومزاده") or msg.text:match("تمار") or msg.text:match("خار") or msg.text:match("کص") or msg.text:match("کس") or msg.text:match("کیر") or msg.text:match("کاندوم") or msg.text:match("شورت") or msg.text:match("ننه") or msg.text:match("کون") or msg.text:match("بیشرف") or msg.text:match("بیناموس") or msg.text:match("بی ناموس") or msg.text:match("کثافت") or msg.text:match("کصافت") or msg.text:match("کسافت") or msg.text:match("اشغال") or msg.text:match("عمه") or msg.text:match("فاک") or msg.text:match("fuck") or msg.text:match("سیک") or msg.text:match("سیکتیر") or msg.text:match("خفه") or msg.text:match("چونی") or msg.text:match("گشاد") or msg.text:match("سولاخی") or msg.text:match("سکسی") or msg.text:match("سکس") or msg.text:match("صکص") or msg.text:match("سکص") or msg.text:match("صکس") or msg.text:match("صکصی") or msg.text:match("گو") or msg.text:match("عن") or msg.text:match("شاش") or msg.text:match("عنتر") or msg.text:match("گوریل") or msg.text:match("نفهم") or msg.text:match("عقب افتاده") or msg.text:match("عوضی") or msg.text:match("عوزی") or msg.text:match("پاره") or msg.text:match("ژنده") or msg.text:match("دزد") or msg.text:match("بی شرف") or msg.text:match("جاکش") or msg.text:match("پوفیوز") or msg.text:match("خایه") or msg.text:match("تخمک") or msg.text:match("پشم") or msg.text:match("ادرار") or msg.text:match("تاپاله") or msg.text:match("گوز") or msg.text:match("چوس") or msg.text:match("قرمساق") or msg.text:match("دیوث") or msg.text:match("دیوس") or msg.text:match("دیوص") or msg.text:match("لاشی") or msg.text:match("کــــــــــیر") or msg.text:match("کـیـــــــــــــــــــــــــــــــــــــــــــــــــــر") or msg.text:match("کــــــــــــــــــــــــــــــیر") or msg.text:match("کــــــــــیرر") or msg.text:match("لاشي") or msg.text:match("ننت") or msg.text:match("جنده") or msg.text:match("jende")
+local is_foshh_msg = msg.text:match("mather") or msg.text:match("father") or msg.text:match("pedar") or msg.text:match("khahar") or msg.text:match("khar") or msg.text:match("mar") or msg.text:match("tamar") or msg.text:match("cos") or msg.text:match("koc") or msg.text:match("coc") or msg.text:match("kos") or msg.text:match("kir") or msg.text:match("cir") or msg.text:match("short") or msg.text:match("kandom") or msg.text:match("kandoom") or msg.text:match("nane") or msg.text:match("maman") or msg.text:match("sharg") or msg.text:match("kod") or msg.text:match("cod") or msg.text:match("hamrah aval") or msg.text:match("irancell") or msg.text:match("raitel") or msg.text:match("nnh") or msg.text:match("nne")
+if group_fosh_lock == 'yes' and is_fosh_msg or is_foshh_msg then
+tg.deleteMessages(msg.chat_id_, {[0] = msg.id_ })
 end
+if is_filter(msg, msg.text) then
+tg.deleteMessages(msg.chat_id_, {[0] = msg.id_ })
+end      
+--[[local group_reply_lock = group[tostring(msg.chat_id)]['settings']['lock_fosh']
+if group_reply_lock == 'yes' and msg.reply_to_message_id_~=0 then
+tg.deleteMessages(msg.chat_id_, {[0] = msg.id_ })
+end]]
 local group_fwd_lock = group[tostring(msg.chat_id)]['settings']['lock_fwd']
 if group_fwd_lock == 'yes' and msg.forward_info_ ~= false then
 tg.deleteMessages(msg.chat_id_, {[0] = msg.id_ })
@@ -113,8 +141,8 @@ return {
   run = run
 }
 
---@mafia_cli
---https://github.com/mafia-007
+--@senator_tea
+
 
 
 
